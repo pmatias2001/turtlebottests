c765f04b3fd2fbf0326b769feb220c030163af34
pmatias2001 <aluno21089@ipt.pt>
2022-09-01 18:41:52 +0100

    houghLines fgksofgs

--- linedetection.py
+++ linedetection.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python 
 import cv2 as cv
 import numpy as np
-import roslib
+import rospy
 import sys
 import math
 from geometry_msgs.msg import Twist
@@ -8,8 +9,8 @@ from geometry_msgs.msg import Twist
 def draw_lines(img, houghLines):
 
     if houghLines is not None:
-        for line in houghLines:
-            for rho,theta in line:
+        #for line in houghLines:
+            for rho,theta in houghLines[0]:
                 a = np.cos(theta)
                 b = np.sin(theta)
                 x0 = a*rho
@@ -23,28 +24,67 @@ def draw_lines(img, houghLines):
     else:
         print("No line detected in image")
 
+def get_theta(img, houghLines):
+
+    if houghLines is not None:
+        #for line in houghLines:
+            for theta in houghLines[0]:
+                return theta
+    else:
+        return 0
+
 def main():
+    rospy.init_node('test', anonymous=True )
+    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    vel_msg = Twist()
+
+    vel_msg.linear.y=0
+    vel_msg.linear.z=0
+    vel_msg.angular.x=0
+    vel_msg.angular.y=0
+
     cap = cv.VideoCapture('lane01.mp4')
 
     while (cap.isOpened()):
         ret, frame = cap.read()
-        #frame_new = cv.resize(frame, (720,480))
-        frame_new = frame[560:700,550:850]
-        if ret==True:
-            gray_image = cv.cvtColor(frame_new, cv.COLOR_RGB2GRAY)
-            canny_image = cv.Canny(gray_image,200,300)
-            lines = cv.HoughLines(canny_image,rho=6,theta=np.pi/60,threshold=200)
-            hough_image = np.zeros_like(frame_new)
-            draw_lines(hough_image,lines)
-            hough_image = cv.addWeighted(frame_new,0.8,hough_image,1.0,0.0)
 
-            cv.imshow('Frame', canny_image)
-            cv.imshow('Frame2', hough_image)
+        vel_msg.linear.x=1.0
+
+        if ret==True:
+            frame_new = frame[560:700,550:850]
+
+            src = np.float32([[0,239],[200,239],[0,0],[200,0]])
+            dst = np.float32([[130,239],[170,239],[0,0],[299,0]])
+            M = cv.getPerspectiveTransform(src,dst)
+
+            warp_image = cv.warpPerspective(frame_new, M, (300,220))
+
+            white_filtro = cv.inRange(warp_image, (230,230,230), (255,255,255))
+
+            result_image = cv.bitwise_and(warp_image,warp_image, mask = white_filtro)
+
+            gray_image = cv.cvtColor(result_image, cv.COLOR_RGB2GRAY)
+
+            canny_image = cv.Canny(gray_image,200,300)
+            lines = cv.HoughLines(canny_image,rho=2,theta=np.pi/90,threshold=150)
+            hough_image = np.zeros_like(warp_image)
+            draw_lines(hough_image,lines)
+            hough_image = cv.addWeighted(warp_image,0.8,hough_image,1.0,0.0)
+
+            if (get_theta!=0):
+                vel_msg.linear.z=(get_theta/180)*math.pi
+                
+            pub.publish(vel_msg)
+
+            cv.imshow ('Normal', white_filtro)
+            cv.imshow('Canny', canny_image)
+            cv.imshow('Lines', hough_image)
+            cv.imshow ('Bird', warp_image)
 
             if cv.waitKey(25) & 0xFF == ord('q'):
                 #cv.imwrite('exemplo',hough_image)
-	            break
-            
+                break
+           
         else: break
 
 if __name__ == '__main__':
